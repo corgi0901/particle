fb12c1e5f7f62f64eee031a8d41bbcce8db641f7
Ryo Yoneyama <ryo.yoneyama.dev@gmail.com>
2018-08-16 08:50:32 +0900

    ドキュメント生成のためのコメントおよび設定ファイルの修正

--- token.h
+++ token.h
@@ -1,30 +1,49 @@
 #ifndef _TOKEN_H_
 #define _TOKEN_H_
 
-// トークンの種類
+/**
+ * トークンの種類
+ */
 typedef enum token_type
 {
-	variable,		 // 変数トークン
-	constants,		 // 定数トークン
-	operation,		 // 演算子トークン
-	unary_operation, // 単項演算子トークン
-	symbol,			 // 記号トークン
+	/// 変数トークン
+	variable,
+	/// 定数トークン
+	constants,
+	/// 算術演算子トークン
+	operation,
+	/// 単項演算子トークン
+	unary_operation,
+	// 記号トークン
+	symbol,
 } token_type;
 
-// トークンの値
+/**
+ * トークンの値
+ */
 typedef union token_value {
+	/// 変数名
 	char name;
+	/// 値
 	int value;
+	/// 演算子
 	char op;
+	/// 記号
 	char symbol;
 } token_value;
 
-// トークン
+/**
+ * トークン列構造体
+ */
 typedef struct token
 {
+	/// トークンの種類
 	token_type type;
+	/// トークンの値
 	token_value value;
+	/// 前方トークン
 	struct token *prev;
+	/// 後方トークン
 	struct token *next;
 } token;
 
