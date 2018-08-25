a356eee16b2195214e50f159129746a411b39880
Ryo Yoneyama <ryo.yoneyama.dev@gmail.com>
2018-08-25 11:19:02 +0900

    デバッグ用のマクロを定義

--- debug.h
+++ debug.h
@@ -4,6 +4,14 @@
 #include "token.h"
 #include "ast.h"
 
+//#define _DEBUG_ON
+
+#ifdef _DEBUG_ON
+#define DPRINTF(x, ...) printf("[DEBUG] " x, __VA_ARGS__)
+#else
+#define DPRINTF(x, ...) ;
+#endif
+
 void printAst(ast_node *, int);
 void printTokens(token *);
 
