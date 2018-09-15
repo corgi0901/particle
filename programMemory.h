d6bb4a5caccea764a3d3d78a8544160d6038f627
Ryo Yoneyama <ryo.yoneyama.dev@gmail.com>
2018-09-16 04:58:47 +0900

    コミット漏れ

--- programMemory.h
+++ programMemory.h
@@ -1,26 +1,11 @@
 #ifndef _PROGRAM_MEM_H_
 #define _PROGRAM_MEM_H_
 
-typedef struct codelist
-{
-	char *code;
-	struct codelist *next;
-} Codelist;
-
-typedef struct programMemory
-{
-	Codelist *head;
-	Codelist *tail;
-	Codelist *current;
-	int pc;
-} ProgramMemory;
-
-void initProgramMemory(ProgramMemory *);
-void releaseProgramMemory(ProgramMemory *);
-
-void store(ProgramMemory *, char *);
-char *fetch(ProgramMemory *);
-void jump(ProgramMemory *, int);
-int getpc(ProgramMemory *);
+void initProgramMemory(void);
+void releaseProgramMemory(void);
+void store(char *);
+char *fetch(void);
+void jump(int);
+int getpc(void);
 
 #endif
